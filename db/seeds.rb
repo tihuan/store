fe023f419f047a2346ddcf233479b93a833a8b00
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-01 20:50:39 -0700

    add seed file for account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ActiveRecord::Base.transaction do
+  5.times do
+      Account.create(
+        fname: Faker::Name.first_name,
+        lname: Faker::Name.last_name,
+        address: Faker::Address.street_address,
+        phone: Faker::PhoneNumber.phone_number,
+        birthdate: Faker::Business.credit_card_expiry_date,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        memo: Faker::Company.bs
+      )
+  end
+end
