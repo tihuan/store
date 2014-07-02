8e8a9e65d31ae038fe1788da4ff3800e3b1cd63e
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-02 16:05:21 -0700

    add methods to appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,30 @@
 class AppointmentsController < ApplicationController
+    def index
+      @appointments = Appointment.all
+    end
+
+    def show
+      @appointment = Appointment.find(params[:id])
+    end
+
+    def new
+      @appointment = Appointment.new
+    end
+
+    def create
+      @appointment = Appointment.new(appointment_params)
+      if @appointment.save
+        flash[:notice] = "Appointment successfully created."
+        redirect_to @appointment
+      else
+        flash[:alert] = "Failed to create appointment. Please try again."
+        render :new
+      end
+    end
+
+  private
+
+    def appointment_params
+      params.require(:appointment).permit!
+    end
 end
