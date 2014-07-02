637c0cd1145088727f4cb81189667b67a739278e
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-01 20:51:04 -0700

    update factory for account

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
   factory :account do |a|
     a.fname { Faker::Name.first_name }
     a.lname { Faker::Name.last_name }
+    a.address { Faker::Address.street_address }
+    a.phone { Faker::PhoneNumber.phone_number }
+    a.birthdate { Faker::Business.credit_card_expiry_date }
+    a.email { Faker::Internet.email }
+    a.password { Faker::Internet.password }
+    a.memo { Faker::Company.bs }
   end
 end
