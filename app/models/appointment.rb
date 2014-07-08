cd1b8535ff2289adf9a80f934d2233e6fc16223f
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-07 22:13:20 -0700

    remove MoneyAttributes from this model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,4 @@
-require "#{Rails.root}/lib/money_attributes"
-
 class Appointment < ActiveRecord::Base
-  include MoneyAttributes
-  money_attributes :price
-
   belongs_to :account
   belongs_to :service
 
