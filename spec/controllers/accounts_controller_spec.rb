2bd054443d7c0663a523645d45ae0bf2a8d39212
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-07 23:09:11 -0700

    Add examples to test #show, #new, render :new if account attributes are invalid

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -5,35 +5,52 @@ describe AccountsController, type: :controller do
   let!(:account2) { FactoryGirl.create(:account) }
 
   describe 'GET #index' do
-    it "assigns all accounts to @accounts" do
+    it 'assigns all accounts to @accounts' do
       get :index
       expect(assigns(:accounts).last).to eq(account2)
     end
 
-    it "renders the :index view" do
+    it 'assigns account to @account' do
+      get :show, id: account1.id
+      expect(assigns(:account)).to eq(account1)
+    end
+
+    it 'assigns account to @account' do
+      get :new
+      expect(assigns(:account).id).to eq(nil)
+    end
+
+    it 'renders the :index view' do
       get :index
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET #show' do
-    it "renders the show page" do
+    it 'renders the show page' do
       get :show, id: account1.id
       expect(response).to render_template(:show)
     end
 
-    it "assigns the requested city to @city" do
+    it 'assigns the requested city to @city' do
       get :show, id: account1.id
       expect(assigns(:account)).to eq(account1)
     end
 end
 
-  describe "POST #create" do
-    it "redirects to show page" do
-      post :create, account: attributes_for(:account)
-      expect(response).to redirect_to(account_path(assigns[:account]))
+  describe 'POST #create' do
+    context 'when account attributes are valid' do
+      it 'redirects to show page' do
+        post :create, account: attributes_for(:account)
+        expect(response).to redirect_to(account_path(assigns[:account]))
+      end
     end
 
-
+    context 'when account attributes are invalid' do
+      it 'renders the new page' do
+        post :create, account: { fname: "Test" }
+        expect(response).to render_template(:new)
+      end
+    end
   end
 end
