824d75d6750c08e0d3084e2883d29e9105f79e28
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-08 09:21:16 -0700

    Add factory for Appointment

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :appointment do
+  factory :appointment do |a|
+    a.date { Faker::Business.credit_card_expiry_date }
+    a.hour { '1200' }
+    a.account_id { rand(2) }
+    a.service_id { rand(2) }
   end
 end
