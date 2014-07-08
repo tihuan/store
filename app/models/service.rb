14c6a0ddb7afefcc657f97bb8c6880ab3c86d7c2
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-07 21:36:35 -0700

    refactor code to use relative path to require money_attributes instead of full path

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,8 @@
+require "money_attributes"
 class Service < ActiveRecord::Base
+  include MoneyAttributes
+  money_attributes :price
+
   has_many :appointments
   has_many :accounts, through: :appointments
 end
