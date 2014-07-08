e0056bdfeca8792a25b8e5d8f6b928017516e1da
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-07 22:48:24 -0700

    Add factory for class Service

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -1,6 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :service do
+  factory :service do |s|
+    s.name { Faker::Commerce.product_name }
+    s.price_in_cents { Faker::Address.building_number }
   end
 end
