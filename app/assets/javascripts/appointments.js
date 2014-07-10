6b395c06664a44ef25cb3e83affa47b9b9ac5254
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-09 18:11:12 -0700

    Remove unnecessary appointment form in Appointment new.html.erb and change related code in appointment.js and controller

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,5 +1,4 @@
 $(function () {
-  $('#appointment_form').hide();
   $('#hour_picker').hide();
   $('#hidden_hour_div').hide();
   $('#cal_previous2').hide();
@@ -30,25 +29,35 @@ $(function () {
   var day = currentTime.getDate();
   var year = currentTime.getFullYear();
 
-  $("#appointment_hour").on('click', function() {
-    if ($("#datepicker").val() !== '') {
+// Show available hours when clicking hour box in #create-appointment-form
+  $('#appointment_hour').on('click', function() {
+    if ($('#datepicker').val() !== '') {
       prevPage();
     }
   });
 
-  $(".cal_table2 td").on('click', function() {
+// Add appointment hour to #appointment_form after selecting time
+  $('.cal_table2').on('click', 'td', function() {
     nextPage($(this).attr('class'), $(this).text());
+    $('#submit_form').show();
   })
 
-  $("#datepicker").datepicker({
+// Add service id to #appointment_form after selecting service
+  // $('#appointment_service_id').on('click', 'option', function() {
+  //   console.log("CLICKED!");
+  //   $('#final_service_name h2').text($(this).text());
+  //   $('#appointments_service_id').val($(this).val());
+  // });
+
+  $('#datepicker').datepicker({
     minDate: new Date(year, month, day),
     dateFormat: 'yy-mm-dd',
     onSelect: function(dateText) {
       setFinalDate = dateText
       hideHourPicker();
       findHours(setFinalDate);
-      $("th.selected_date").empty();
-      $("th.selected_date").append("Date Chosen:" + " " + (setFinalDate));
+      $('th.selected_date').empty();
+      $('th.selected_date').append('Date Chosen:' + ' ' + (setFinalDate));
       pageNo = '2';
     }
   });
@@ -56,14 +65,14 @@ $(function () {
 
 function findHours(chosen_date){
   $.ajax({
-    url: "/appointments",
+    url: '/appointments',
     cache: false,
-    dataType: "json",
+    dataType: 'json',
     data: { matched_date:chosen_date },
     success: function(resp){
       $('#hour_picker').hide();
       var hours_string = resp;
-      $(".cal_table2 td").removeAttr("style");
+      $('.cal_table2 td').removeAttr('style');
       for (var j in hours_string) {
         (final_array = '\.'+ hours_string[j]);
         $(final_array).hide();
@@ -75,31 +84,29 @@ function findHours(chosen_date){
 
 function nextPage(hour, id){
   $('#hour_picker').hide();
-  $("#appointment_form").show();
   $('#cal_previous2').show();
-  document.getElementById('subData').style.display = 'block'
   setDateAndHour(hour, id);
   pageNo = '3';
   return true;
 };
 
 function prevPage() {
-  setFinalHour = "";
+  setFinalHour = '';
   hideHourPicker();
-  $("#hour_picker").show();
+  $('#hour_picker').show();
   pageNo = '2'
 };
 
 function hideHourPicker() {
-  $('#appointment_form').hide();
   $('#cal_previous2').hide();
   $('#appointment_hour').val('');
+  $('#submit_form').hide();
 }
 
 function setDateAndHour(hour, id) {
-  $('#final_date h2').text(setFinalDate);
-  $('#final_hour h2').text(id);
   $('#appointment_hour').val(id);
-  $('#appointments_hour').val(hour);
-  $('#schedules_date').val(setFinalDate);
+  // $('#final_date h2').text(setFinalDate);
+  // $('#final_hour h2').text(id);
+  // $('#appointments_hour').val(hour);
+  // $('#appointments_date').val(setFinalDate);
 }
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,12 +4,13 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.all
     date_from_ajax = params[:matched_date]
     reduce = Appointment.where(:date => date_from_ajax)
-    @new_dates = reduce.collect {|x| x.hour}
+    @new_dates = reduce.collect {|x| x.hour.gsub(/\D/, '').rjust(4, "0")}
     render json: @new_dates
   end
 
   def new
     @appointment = Appointment.new
+    @service_names = Service.all.each_with_index.map { |service, i| [service.name, i] }
     respond_to do |format|
       format.html
       format.js
@@ -17,6 +18,8 @@ class AppointmentsController < ApplicationController
   end
 
   def create
+    puts "PARAMS HERE\n\n\n\n\n"
+    p appointment_params
     @appointment = Appointment.create(appointment_params)
     if @appointment.save
       redirect_to new_appointment_path
@@ -33,6 +36,6 @@ class AppointmentsController < ApplicationController
 private
 
   def appointment_params
-    params.require(:appointments).permit!
+    params.require(:appointment).permit!
   end
 end
